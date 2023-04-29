5833ec7797ee662024710434001829e476f5c46e
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2023-04-29 17:26:02 -0400

    Adding code for operating procedures

--- init_db.py
+++ init_db.py
@@ -16,6 +16,48 @@ with open('schema.sql') as f:
 # execute the SQL commands in the file
 cur.execute(sql)
 
+
+def load_text_file_into_operatingexpenditures(fileName, sqlInsert, cur):
+     # Open the first text file and insert data into the table
+    cnt=0
+    with open(fileName, 'r') as f:
+        subid=1
+        for line in f:
+                data = line.strip().split('|')
+                print(data)
+                print(len(data))
+                if "pac" in fileName:
+                     modifieddata=modifypacdata(data)
+                     print(len(modifieddata))
+                     cur.execute(
+                sqlInsert,
+                modifieddata
+                )
+                indices_to_remove = [7, 8, 9, 10]
+                data = [data[i] for i in range(len(data)) if i not in indices_to_remove]
+                #data=data[0:12]
+                #data[11]='1'
+               # data[10]='1'
+                #data[9]='1'
+                #data.append(1)
+                indices_to_remove = [20,21]
+                data = [data[i] for i in range(len(data)) if i not in indices_to_remove]
+                data[6]=1
+                data[16]=subid
+                data[5]=subid
+                subid=subid+1
+                print(data)
+                
+                #print(data[1]+' '+data[2]+' '+data[3]+' '+data[4])
+                #del data[1:4]
+                print(len(data))
+                print(data)
+                #modifieddata=modifydata(data)
+                #print(data)
+                cur.execute(
+                sqlInsert,
+                data
+                )
 def load_text_file_into_indpendentexpenditures(fileName,sqlInsert,cur):
      cnt=0
      with open(fileName, 'r') as f:
@@ -126,6 +168,7 @@ try:
     sql_insert_contributor="INSERT INTO contributor(CONTRIBUTOR_ID,NAME,CITY ,STATE , ZIP_CODE ,EMPLOYER ,OCCUPATION ) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     sql_insert_pacpartysummary="INSERT INTO pacandpartysummary(CMTE_ID,TTL_RECEIPTS, TRANS_FROM_AFF, INDV_CONTRIB, OTHER_POL_CMTE_CONTRIB , CAND_CONTRIB , CAND_LOANS , TTL_LOANS_RECEIVED , TTL_DISB, TRANF_TO_AFF, INDV_REFUNDS , OTHER_POL_CMTE_REFUNDS , CAND_LOAN_REPAY , LOAN_REPAY , COH_BOP , COH_COP , DEBTS_OWED_BY , NONFED_TRANS_RECEIVED , CONTRIB_TO_OTHER_CMTE , IND_EXP , PTY_COORD_EXP , NONFED_SHARE_EXP , CVG_END_DT) VALUES (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s,%s)"
 
+    sql_insert_operatingexpenditures="INSERT INTO operatingexpenditures(CMTE_ID  , AMNDT_IND , RTP_YR , RPT_TP , LINE_NUM , SCHED_TP_CD , CONTRIBUTOR_ID , TRANSACTION_DT , TRANSACTION_AMT , TRANSACTION_PGI , PURPOSE , CATEGORY , CATEGORY_DESC , MEMO_CD , MEMO_TEXT , ENTITY_TP , SUB_ID , FILE_NUM , TRAN_ID , BACK_REF_TRAN_ID ) VALUES (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s, %s, %s)"
     sql_insert_independentexpenditures="INSERT INTO independentexpenditures(CMTE_ID, AMNDT_IND , RPT_TP , TRANSACTION_PGI , TRANSACTION_TP, ENTITY_TP , CONTRIBUTOR_ID, TRANSACTION_DT , TRANSACTION_AMT, OTHER_ID , TRAN_ID , MEMO_CD, SUB_ID ) VALUES (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s)"
     sql_insert_houseandsenate="INSERT INTO currentcampaignforhouseandsenate(CAND_ID,PTY_CD, TTL_RECEIPTS ,TRANS_FROM_AUTH , TTL_DISB , TRANS_TO_AUTH , COH_BOP , COH_COP , CAND_CONTRIB , CAND_LOANS , OTHER_LOANS, CAND_LOAN_REPAY , OTHER_LOAN_REPAY , DEBTS_OWED_BY , TTL_INDIV_CONTRIB,SPEC_ELECTION , PRIM_ELECTION , RUN_ELECTION , GEN_ELECTION , GEN_ELECTION_PRECENT , OTHER_POL_CMTE_CONTRIB, POL_PTY_CONTRIB , CVG_END_DT , INDIV_REFUNDS , CMTE_REFUNDS) VALUES (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)"
     load_text_file_into_db('data/cn.txt', sql_insert_candidate, cur)
@@ -133,6 +176,7 @@ try:
     load_text_file_into_db('data/ccl.txt', sql_insert_candidate_committees, cur)
     load_text_file_into_houseandsenate('data/houseandsenate.txt', sql_insert_houseandsenate,cur)
     load_text_file_into_indpendentexpenditures('data/independentexpenditures.txt',sql_insert_independentexpenditures,cur)
+    load_text_file_into_operatingexpenditures('data/operatingexpenditures.txt',sql_insert_operatingexpenditures,cur)
 
     # Commit the changes and close the connection
     conn.commit()
